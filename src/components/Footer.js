96ae1eccac338450dddc9e413c5fa53214f28210
kasiorzynka <katarzyna.ficz@gmail.com>
2024-11-12 01:07:56 +0100

    Styles changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
   <Segment className="app-footer" inverted>
     <LogoFull color="#ffffff" />
     <Container textAlign="center">
-      <a href="https://www.linkedin.com/in/katarzyna-ficz/">&copy; {new Date().getFullYear()} Ficzer Company</a>
+      <a href="https://www.linkedin.com/in/katarzyna-ficz/" target="_blank" rel="noreferrer">&copy; {new Date().getFullYear()} Ficzer Company</a>
       <p>All rights reserved</p>
     </Container>
   </Segment>
