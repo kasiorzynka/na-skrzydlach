c1aca47ae0c1542630ec1edca036187d043f46b3
kasiorzynka <katarzyna.ficz@gmail.com>
2024-11-15 17:33:29 +0100

    Added full photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,18 @@
 import { Container, Header } from "semantic-ui-react";
-// import photo from "../Agnieszka_Twarowska.jpg";
+import PhotoFull from "../photos/Agnieszka-Twarowska-full.jpg";
 
 const Home = () => {
   return (
     <div>
       <Container text className="home">
-        {/*
-        <img
-          src={photo}
-          alt="Agnieszka Twarowska"
-          style={{ maxWidth: "200px" }}
-  />*/}
+
         <Header as="h1">Agnieszka Twarowska</Header>
         <Header>Gabinet psychoterapii &quot;Na skrzydłach&quot;</Header>
+        <img
+          src={PhotoFull}
+          alt="Agnieszka Twarowska"
+          className="photo-full"
+        />
         <p>Jestem magistrem psychologii oraz certyfikowanym terapeutą poznawczo – behawioralnym (nr&nbsp;certyfikatu 1454).</p>
         <p>
           W zawodzie pracuję od 2012r. Początkowo zajmowałam się interwencją kryzysową i poradnictwem psychologicznym. Obecnie w ramach prywatnej praktyki prowadzę indywidualne procesy psychoterapeutyczne. Pracuję też w Oddziale Dziennym Ogólnopsychiatrycznym Mazowieckiego Szpitala Bródnowskiego, gdzie prowadzę psychoterapię grupową, konsultacje indywidualne oraz spotkania z rodzinami pacjentów. W ramach swoich obowiązków wdrażam i nadzoruję pracę psychologów realizujących szpitalny staż. Współpracuję z Warszawskim Uniwersytetem Medycznym prowadząc zajęcia dla studentów uniwersyteckiej sekcji zagranicznej z tematyki diagnozy psychologicznej.
